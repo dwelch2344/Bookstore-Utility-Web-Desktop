90220c563aee0fe1efd6c19e2d65ebb95cf9c779
dwelch <dwelch@192.168.1.100>
2009-12-07 10:43:24 -0700

    Simple Grid Demo up and running

--- src/edu/slcc/bookstore/utility/client/EntryPoint.java
+++ src/edu/slcc/bookstore/utility/client/EntryPoint.java
@@ -2,24 +2,17 @@ package edu.slcc.bookstore.utility.client;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import com.extjs.gxt.desktop.client.Desktop;
-import com.extjs.gxt.ui.client.Style.SortDir;
-import com.extjs.gxt.ui.client.data.BaseModel;
-import com.extjs.gxt.ui.client.data.BaseModelData;
-import com.extjs.gxt.ui.client.data.BasePagingLoadConfig;
-import com.extjs.gxt.ui.client.data.Loader;
+import com.extjs.gxt.ui.client.data.BeanModel;
+import com.extjs.gxt.ui.client.data.BeanModelFactory;
+import com.extjs.gxt.ui.client.data.BeanModelLookup;
 import com.extjs.gxt.ui.client.data.ModelData;
-import com.extjs.gxt.ui.client.data.PagingLoadConfig;
 import com.extjs.gxt.ui.client.store.ListStore;
 import com.extjs.gxt.ui.client.widget.Window;
 import com.extjs.gxt.ui.client.widget.grid.ColumnConfig;
 import com.extjs.gxt.ui.client.widget.grid.ColumnModel;
 import com.extjs.gxt.ui.client.widget.grid.Grid;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.user.client.ui.Button;
 
 import edu.slcc.bookstore.utility.client.data.Modelable;
 import edu.slcc.bookstore.utility.client.domain.Employee;
@@ -29,36 +22,42 @@ public class EntryPoint implements com.google.gwt.core.client.EntryPoint {
 	private Desktop desktop = new Desktop();
 	@Override
 	public void onModuleLoad() {
-		Window win = getWindow();
+		Window win = getWindow2();
 		desktop.addWindow(win);
 		win.show();
 	}
-
 	
-	public Window getWindow(){
+
+	public Window getWindow2(){
 		Window win = new Window();
+		
+		// This lets us use our Beans in our Widgets. 
+		// The factory creates BeanModel instances from our Bean Class
+		BeanModelFactory factory = BeanModelLookup.get().getFactory(Employee.class);
+		
 		// Create the store and add our models to it
-		final ListStore<ModelData> store = new ListStore<ModelData>();
-		for(Modelable m : getList()){
-			store.add(m.getModel());
+		final ListStore<BeanModel> store = new ListStore<BeanModel>();
+		for(Employee m : getBeanList()){
+			BeanModel model = factory.createModel(m);
+			store.add(model);
 		}
 		
 		// Create our columns
 		List<ColumnConfig> columns = new ArrayList<ColumnConfig>();
-		ColumnConfig column = new ColumnConfig("sNumber", "SNumber", 100);
-		columns.add(column);
-		column = new ColumnConfig("name", "Employee", 100);
-		columns.add(column);
+		columns.add(new ColumnConfig("sNumber", 100));
+		columns.add(new ColumnConfig("name", 100));
 		ColumnModel model = new ColumnModel(columns);
-		Grid<ModelData> grid = new Grid<ModelData>(store, model);
+		
+		// Create the Grid from the data and the columns
+		Grid<BeanModel> grid = new Grid<BeanModel>(store, model);
 		win.add(grid);
 		
 		return win;
 	}
 	
 	
-	private List<Modelable> getList(){
-		List<Modelable> list = new ArrayList<Modelable>();
+	private List<Employee> getBeanList(){
+		List<Employee> list = new ArrayList<Employee>();
 		list.add(new Employee("S00456789", "David Welch"));
 		list.add(new Employee("S00456790", "Susan White"));
 		list.add(new Employee("S00456791", "Richard Allred"));
--- src/edu/slcc/bookstore/utility/client/domain/Employee.java
+++ src/edu/slcc/bookstore/utility/client/domain/Employee.java
@@ -1,17 +1,32 @@
 package edu.slcc.bookstore.utility.client.domain;
 
+import java.io.Serializable;
+
 import com.extjs.gxt.ui.client.data.BaseModelData;
+import com.extjs.gxt.ui.client.data.BeanModelMarker;
+import com.extjs.gxt.ui.client.data.BeanModelTag;
 import com.extjs.gxt.ui.client.data.ModelData;
+import com.extjs.gxt.ui.client.data.BeanModelMarker.BEAN;
 
 import edu.slcc.bookstore.utility.client.data.Modelable;
 
-public class Employee implements Modelable{
+public class Employee implements Serializable, BeanModelTag, Modelable{
 
+	private static final long serialVersionUID = -7413481463818422595L;
 	private String sNumber, name;
 	
 	public Employee(String sNumber, String name) {
 		this.sNumber = sNumber;
 		this.name = name;
+	}
+	
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getsNumber() {
+		return sNumber;
 	}
 	
 	@Override
@@ -25,4 +40,14 @@ public class Employee implements Modelable{
 			set("name", name);
 		}
 	}
+	
+	
+	/*
+	 * Used to tag this JavaBean for use as a BeanModel. See 
+	 * http://www.extjs.com/helpcenter/index.jsp?topic=/com.extjs.gxt.help/html/tutorials/beanmodel.html
+	 *
+	@BEAN(Employee.class)
+	public interface EmployeeBeanModel extends BeanModelMarker{
+		
+	}*/
 }
